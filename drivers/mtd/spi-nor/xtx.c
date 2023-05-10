d0e157f1ae8177c95689ac1b224b5bfc34959bd1
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2023-05-10 18:57:23 +0800

    mtd: spi-nor: add flash info for XT25F32B
    This is used on ROCK 4.
    
    Signed-off-by: Yuntian Zhang <yt@radxa.com>

--- drivers/mtd/spi-nor/xtx.c
+++ drivers/mtd/spi-nor/xtx.c
@@ -8,6 +8,9 @@
 #include "core.h"
 
 static const struct flash_info xtx_parts[] = {
+	{ "XT25F32B", INFO(0x0b4016, 0, 64 * 1024, 64)
+				FLAGS(SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ)
+				NO_SFDP_FLAGS(SECT_4K) },
 	{ "XT25F64F", INFO(0x0b4017, 0, 64 * 1024, 128)
 			    FLAGS(SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ)
 			    NO_SFDP_FLAGS(SECT_4K) },
